4d6252cc9531ba4ba3a674dfb1d49fdf52f2eceb
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-07-16 19:30:36 +0900

    mount notFound handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import errorHandler from './middleware/errorHandler';
+import NotFound from './middleware/notFound';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('*', NotFound);
 
 //error handler
 app.use(errorHandler);
