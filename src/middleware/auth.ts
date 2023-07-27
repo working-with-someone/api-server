4c6000171646a6717d4a9b4c6b2cc34c62bc9e45
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-07-28 00:43:58 +0900

    modify : move jwt from cookie to header

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,19 +2,27 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import httpStatusCode from 'http-status-codes';
 import { wwsError } from '../utils/wwsError';
-import asyncCatch from '../utils/asyncCatch';
 
-const authMiddleware = asyncCatch(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const userToken = req.cookies?.user;
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split('Bearer ')[1];
 
-    if (
-      userToken &&
-      jwt.verify(userToken, process.env.TOKEN_USER_SECRET as string)
-    ) {
+  if (token) {
+    try {
+      jwt.verify(token, process.env.TOKEN_USER_SECRET as string);
       return next();
+    } catch (err) {
+      return next(
+        new wwsError(
+          httpStatusCode.UNAUTHORIZED,
+          httpStatusCode.getStatusText(httpStatusCode.UNAUTHORIZED)
+        )
+      );
     }
-
+  } else {
     return next(
       new wwsError(
         httpStatusCode.UNAUTHORIZED,
@@ -22,6 +30,6 @@ const authMiddleware = asyncCatch(
       )
     );
   }
-);
+};
 
 export { authMiddleware };
