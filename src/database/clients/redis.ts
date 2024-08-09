68df4e8875d17cb0952899b91778509fd119593c
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-08-09 20:52:28 +0900

    update : print redisClient event

--- src/database/clients/redis.ts
+++ src/database/clients/redis.ts
@@ -3,4 +3,18 @@ import redisClientConfig from '../../config/redisClient.config';
 
 const redisClient = createClient(redisClientConfig);
 
+redisClient.on('ready', () => {
+  console.log(
+    `✅  Redis client connected on port ${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+  );
+});
+
+redisClient.on('error', (err) => {
+  console.error('❌ redis client connection error ', err);
+});
+
+redisClient.on('end', () => {
+  console.log(`✅  Redis client disconnected`);
+});
+
 export default redisClient;
