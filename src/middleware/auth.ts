38ff24283f9aff3c418c02e18c580aa20c385ba7
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-08-13 18:43:01 +0900

    update authorization middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
 import httpStatusCode from 'http-status-codes';
 import { wwsError } from '../utils/wwsError';
 
@@ -8,28 +7,16 @@ const authMiddleware = async (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.headers.authorization?.split('Bearer ')[1];
-
-  if (token) {
-    try {
-      jwt.verify(token, process.env.TOKEN_USER_SECRET as string);
-      return next();
-    } catch (err) {
-      return next(
-        new wwsError(
-          httpStatusCode.UNAUTHORIZED,
-          httpStatusCode.getStatusText(httpStatusCode.UNAUTHORIZED)
-        )
-      );
-    }
-  } else {
-    return next(
-      new wwsError(
-        httpStatusCode.UNAUTHORIZED,
-        httpStatusCode.getStatusText(httpStatusCode.UNAUTHORIZED)
-      )
-    );
+  if (req.session.userId) {
+    return next();
   }
+
+  return next(
+    new wwsError(
+      httpStatusCode.UNAUTHORIZED,
+      httpStatusCode.getStatusText(httpStatusCode.UNAUTHORIZED)
+    )
+  );
 };
 
 export { authMiddleware };
