fbc9a677d27c38a0969c325d8b9fb3660d49bf65
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-08-13 18:51:30 +0900

    update : set test session

--- tests/integration/user.test.ts
+++ tests/integration/user.test.ts
@@ -1,19 +1,33 @@
 import prismaClient from '../../src/database/clients/prisma';
 import testUserData from '../data/user.json';
-import server from '../../src/server';
+import redisClient from '../../src/database/clients/redis';
+import cookie from 'cookie';
+import { sessionIdName } from '../../src/config/session.config';
 
 describe('User API', () => {
+  const currUser: Record<string, any> = testUserData.users[0];
+  currUser.sidCookie = cookie.serialize(
+    sessionIdName,
+    's:swUe4NER4JK-dXjjJ6BBh9ror_fVThwL.DdAmHH/rbIlEXTUYisnFX0mPit8jB9AtOyAXjmtO7jo'
+  );
+
   beforeAll(async () => {
     testUserData.users.forEach(async (user) => {
       await prismaClient.user.create({
         data: user,
       });
     });
+
+    await redisClient.connect();
+
+    await redisClient.set(
+      'sess:swUe4NER4JK-dXjjJ6BBh9ror_fVThwL',
+      '{"cookie":{"originalMaxAge":null,"expires":null,"secure":false,"httpOnly":true, "path":"/"},"userId":1}'
+    );
   });
 
   afterAll(async () => {
     await prismaClient.user.deleteMany({});
-
-    server.close();
+    await redisClient.disconnect();
   });
 });
