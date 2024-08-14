0289deb9666c4d5842a8012dee6f08cd7d6b4ef9
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-08-14 14:52:48 +0900

    add : controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express';
 import asyncCatch from '../utils/asyncCatch';
+import { userService } from '../services';
 
+// 사용자의 공개가능한 정보만을 가져온다
 export const getUser = asyncCatch(async (req: Request, res: Response) => {
-  return;
+  const publicUserInfo = await userService.getUser(
+    // contoller로 넘어왔다면, user api validation에서 req.params.userId가 number인 것이 검증된다.
+    parseInt(req.params.userId)
+  );
+
+  return res.status(200).json({ user: publicUserInfo });
 });
 
 export const updateUser = asyncCatch(async (req: Request, res: Response) => {
