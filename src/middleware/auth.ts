0a0bbc357b8005c5faf7ec0b59f9587c1b822704
seungho-hub <kmc54320@gmail.com>
2024-09-12 13:16:12 +0900

    fix : check user existance in authentication middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import httpStatusCode from 'http-status-codes';
 import { wwsError } from '../utils/wwsError';
+import prismaClient from '../database/clients/prisma';
 
 const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (req.session.userId) {
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: req.session.userId,
+    },
+  });
+
+  if (user) {
     return next();
   }
 
