9eccdd86e587bf7f4b1bafacd1ac3136f3a439c8
seungho-hub <kmc54320@gmail.com>
2024-09-21 16:56:06 +0900

    add comment

--- tests/jest/tearDown.ts
+++ tests/jest/tearDown.ts
@@ -12,13 +12,18 @@ import {
   ObjectIdentifier,
 } from '@aws-sdk/client-s3';
 
+/**
+ * test과정에서 upload한 과정에서 s3 test storage에 upload한 모든 object들을 제거한다.
+ */
 async function deleteAllUploadedS3Object() {
+  // test storage에 존재하는 모든 object를 가져온다.
   const getObjectKeys = new ListObjectsCommand({
     Bucket: process.env.AWS_BUCKET_NAME,
   });
 
   const res = await s3Client.send(getObjectKeys);
 
+  // object가 존재한다면, object들을 모두 제거한다.
   if (res.Contents) {
     const deleteObjectsCommand = new DeleteObjectsCommand({
       Bucket: process.env.AWS_BUCKET_NAME,
