e5204859a46fdb6e0d75d0a88aede3ab0860a513
seungho-hub <kmc54320@gmail.com>
2024-12-05 19:59:13 +0900

    add conflict check in follow creation

--- src/services/follow.service.ts
+++ src/services/follow.service.ts
@@ -51,6 +51,16 @@ export async function createFollow(data: CreateFollow) {
     throw new wwsError(404, 'can not found target user');
   }
 
+  const alreadyFollow = await prismaClient.follow.findUnique({
+    where: {
+      follower_user_id_following_user_id: data,
+    },
+  });
+
+  if (alreadyFollow) {
+    throw new wwsError(httpStatusCode.CONFLICT, 'already following that user');
+  }
+
   // follow record 생성과 user들의 following_count increment, follower count increment가 문제 없이 실행된다면
   // 생성된 follow record를 반환한다.
   try {
