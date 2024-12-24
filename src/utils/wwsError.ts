ac087e01e46e9e53910b41fd5a3d132ac0a8cceb
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-12-24 13:27:38 +0900

    add tsdoc

--- src/utils/wwsError.ts
+++ src/utils/wwsError.ts
@@ -1,5 +1,11 @@
 import httpStatusCode from 'http-status-codes';
 
+/**
+ *
+ * @param status - HTTP status code.
+ * @param message - Error message to respond to the client. If not provided, the status text corresponding to the status code will be used as the default response.
+ * @param err - If the original Error is included, it will be treated as an unhandled exception in the error handler, and a 500 status code will be returned.
+ */
 class wwsError extends Error {
   status: number;
   statusText: string;
