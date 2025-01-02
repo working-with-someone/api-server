047569e86245ff7b39d02add4c68c0bc950330c4
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-01-02 15:43:35 +0900

    add session permission check middleware

--- src/middleware/permission.ts
+++ src/middleware/permission.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import httpStatusCode from 'http-status-codes';
 import { wwsError } from '../utils/wwsError';
+import prismaClient from '../database/clients/prisma';
 
 interface PermissionMiddlewareOption {
   block: boolean;
@@ -15,3 +16,21 @@ export const userPermission =
 
     next();
   };
+
+export const sessionPermission = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const session = await prismaClient.session.findUnique({
+    where: {
+      id: req.params.live_session_id,
+    },
+  });
+
+  if (req.session.userId !== session?.organizer_id) {
+    return next(new wwsError(httpStatusCode.FORBIDDEN));
+  }
+
+  return next();
+};
