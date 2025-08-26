47bd54231f6972b039927b740eba2412dd5eb274
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2025-08-26 21:58:44 +0900

    fix : typo

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -4,13 +4,13 @@ import { categoryService } from '../services/';
 import { CategorySortKey } from '../services/category.service.d';
 
 export const getCategories = asyncCatch(async (req: Request, res: Response) => {
-  const follows = await categoryService.getCategories({
+  const categories = await categoryService.getCategories({
     per_page: parseInt(req.query.per_page as string),
     page: parseInt(req.query.page as string),
     sort: req.query.sort as CategorySortKey,
   });
 
   return res.status(200).json({
-    data: follows,
+    data: categories,
   });
 });
